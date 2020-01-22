03e38fde9f68e4f84ad3a20bbb5ed34d24799ad2
tejaswigowda <tejaswi@foxyninjastudios.com>
2020-01-22 11:16:13 -0700

    in class

--- evalServer/server.js
+++ evalServer/server.js
@@ -12,7 +12,8 @@ app.get("/", function (req, res) {
 
 
 app.get("/eval", function (req, res) {
-
+  console.log(req.query.a);
+    result = eval(req.query.a);
     res.send(result.toString()); // send response body
 });
 
