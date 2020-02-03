5df62b1a6908588527a60c4065e235ea6c9aa690
tejaswigowda <tejaswi@foxyninjastudios.com>
2020-02-03 15:49:52 -0700

    in class

--- basicServers/staticServer.js
+++ basicServers/staticServer.js
@@ -1,24 +1,24 @@
 var express = require("express");
-var app = express();
+var server = express();
 var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
 var port = 8081;
 
-app.get("/", function (req, res) {
+server.get("/", function (req, res) {
       res.redirect("/index.html");
 });
 
-app.get("/hello", function (req, res) {
+server.get("/hello", function (req, res) {
     var name = req.query.name;
     res.send("hello "+ name); // send response body
 });
 
-app.use(methodOverride());
-app.use(bodyParser());
-app.use(express.static(__dirname + '/'));
-app.use(errorHandler());
+server.use(methodOverride());
+server.use(bodyParser());
+server.use(express.static(__dirname + '/'));
+server.use(errorHandler());
 
 console.log("Simple static server listening at http://" + hostname + ":" + port);
-app.listen(port);
+server.listen(port);
