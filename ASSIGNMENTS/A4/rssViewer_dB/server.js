c584eacf4bee9bb2019e576aa2b9c2c2fc8d1d2a
Ubuntu <ubuntu@ip-172-30-0-156.us-west-2.compute.internal>
2020-02-17 18:38:55 +0000

    ac

--- ASSIGNMENTS/A4/rssViewer_dB/server.js
+++ ASSIGNMENTS/A4/rssViewer_dB/server.js
@@ -4,7 +4,7 @@ var bodyParser = require('body-parser');
 var errorHandler = require('errorhandler');
 var methodOverride = require('method-override');
 var hostname = process.env.HOSTNAME || 'localhost';
-var port = 1234;
+var port = 8080;
 var MS = require("mongoskin");
 
 var db = MS.db("mongodb://127.0.0.1:27017/rssParser")
