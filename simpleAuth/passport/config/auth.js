299c7bab940c3c0a7fdefa568e5e57057910b07f
tejaswigowda <tejaswi@foxyninjastudios.com>
2020-02-26 08:31:26 -0700

    ac

---
+++ simpleAuth/passport/config/auth.js
@@ -0,0 +1,21 @@
+module.exports = {
+
+    'facebookAuth' : {
+        'clientID'        : '752321568224006',
+        'clientSecret'    : '345b747e40500f7e430f231c0de6bbf7',
+        'callbackURL'     : 'http://localhost:8080/auth/facebook/callback'
+    },
+
+    'twitterAuth' : {
+        'consumerKey'        : 'hbtHqVYOoa339hpLzu2hpZ2yn',
+        'consumerSecret'     : 'r4abzy2j0BgXyNQqccy3IB63jVUGwDGUUdnUfOMYJscVEKpwW8',
+        'callbackURL'        : 'http://localhost:8080/auth/twitter/callback'
+    },
+
+    'googleAuth' : {
+        'clientID'         : '94890423367-fbk6retha0b2m1ial22e9fehgie5p1dc.apps.googleusercontent.com',
+        'clientSecret'     : '_2Dq_T1EhuzBxU7znlKFPV6P',
+        'callbackURL'      : 'http://localhost:8080/auth/google/callback'
+    }
+
+};
---
+++ simpleAuth/server.js
@@ -0,0 +1,64 @@
+var url = require("url"),
+	querystring = require("querystring");
+
+var passport = require('passport');
+var fs = require('fs');
+var path = require('path'),
+  express = require('express'),
+  db = require('mongoskin').db('mongodb://127.0.0.1:27017/test');
+
+
+var mongoose = require('mongoose');
+var configDB = require('./passport/config/database.js');
+mongoose.connect(configDB.url); // connect to our database
+
+var app = express();
+var secret = 'test' + new Date().getTime().toString()
+
+var session = require('express-session');
+app.use(require("cookie-parser")(secret));
+var MongoStore = require('connect-mongo')(session);
+app.use(session( {store: new MongoStore({
+   url: 'mongodb://127.0.0.1:27017/test',
+   secret: secret
+})}));
+app.use(passport.initialize());
+app.use(passport.session());
+var flash = require('express-flash');
+app.use( flash() );      
+
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+app.use(methodOverride());
+app.use( bodyParser.json() );       // to support JSON-encoded bodies
+app.use(bodyParser.urlencoded({     // to support URL-encoded bodies
+  extended:false 
+}));
+require('./passport/config/passport')(passport); // pass passport for configuration
+require('./passport/routes.js')(app, passport); // load our routes and pass in our app and fully configured passport
+
+
+
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.get("/getsomeDBinfo", isLoggedIn, function(req, res){ //Make sure user is logged in before callback is executed
+    var userID = req.user.local.email; // Know exactly which user you are dealing with
+    res.send("sbvksjd")
+})
+
+app.listen(8080);
+
+
+
+
+
+// route middleware to ensure user is logged in
+function isLoggedIn(req, res, next) {
+    if (req.isAuthenticated())
+        return next();
+
+    res.send('noauth');
+}
+
+
